ed22412513128c7aab1f3d1a481350db2b99007e
Eleonora2502 <ellombardo@outlook.it>
2025-11-07 09:47:12 +0100

    Creazione classe RichiestaStorico

--- src/Shared/RichiestaStorico.java
+++ src/Shared/RichiestaStorico.java
@@ -1,5 +1,24 @@
 package Shared;
 
-public class RichiestaStorico {
-    
+public class RichiestaStorico extends Richiesta {
+    private String nome, cognome, email;
+
+    public RichiestaStorico (String nome, String cognome, String email){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCognome(){
+        return cognome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
 }
