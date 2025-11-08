75f93032aaf4e0e3225cba32e5fb44d3672afe4e
justpeppe <trimarchig2002@gmail.com>
2025-11-08 09:36:52 +0100

    Server.java fatto, controllare se presenta errori dopo la creazione di GestoreClient.java

--- src/ServerSide/Server.java
+++ src/ServerSide/Server.java
@@ -1,14 +1,28 @@
 package ServerSide;
 
+import Shared.Utils;
 import Shared.Utils.*;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.management.RuntimeErrorException;
+
 public class Server {
 
+    public static void main(String[] args) {
+        int porta = Integer.parseInt(Utils.getElementValue("server.xml", "porta"));
+        ServerSocket serverSocket;
 
-    public
-
+        try {
+            serverSocket = new ServerSocket(porta);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new GestoreClient(socket).start;
+            }
+        } catch (IOException ioException) {
+            throw new RuntimeErrorException(ioException);
+        }
+    }
 }
