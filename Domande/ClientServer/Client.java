f592b8601eb1457bdf8e6bbf16f9828ae1f47d78
justpeppe <trimarchig2002@gmail.com>
2025-11-14 18:17:24 +0100

    prima domanda fatta

---
+++ Domande/ClientServer/Client.java
@@ -0,0 +1,33 @@
+package Domande.ClientServer;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+    public static void GestioneClient() {
+
+        int porta = 8080;
+        String host = "localhost";
+
+        try {
+
+            Socket socket = new Socket(host, porta);
+
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            printWriter.println("Pubblica questo messaggio");
+            String risposta = bufferedReader.readLine();
+
+            System.out.println("Il server ha risposto cosi: " + risposta);
+
+            socket.close();
+
+        } catch (IOException e) {
+            System.err.println("Errore: " + e.getMessage());
+        }
+
+    }
+
+}
---
+++ Domande/ClientServer/Server.java
@@ -0,0 +1,33 @@
+package Domande.ClientServer;
+
+// Importazione pacchetti e librerie
+import java.net.*;
+import java.io.*;
+
+// Creazione classe Server
+public class Server {
+
+    // Creazione metodo avvia server
+    public static void AvviaServer() {
+
+        int porta = 8080;
+
+        try {
+
+            // Creazione del serversocket sulla porta dichiarata
+            ServerSocket serverSocket = new ServerSocket(porta);
+
+            // Accettazione richieste tramite serversocket.accept
+            Socket socket = serverSocket.accept();
+
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            // Lancio delle eccezioni
+            System.err.println("Errore " + e.getMessage());
+        }
+
+    }
+
+}
